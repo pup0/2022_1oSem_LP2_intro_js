06a02e7bf7e8c60da3725bb53147d76bd7d2073a
pup0 <79257963+pup0@users.noreply.github.com>
2022-02-19 12:44:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+
+var idade = 18
+console.log("Oi, " + nome)
+if (idade >= 18){
+    var nome = "Cleitão"
+    console.log ("Parabéns, " + nome)
+    console.log("Você pode andar!")
+}
+console.log("Até logo! " + nome)
+
+
+// var linguagem = "Javascript"
+// console.log ("Aprendendo" + linguagem)    //var. pode ser redeclarada
+
+// var linguagem = "Java"
+// console.log("Aprendendo" + linguagem)
+
+// const nome =  'Josias'
+// const idade = 24
+// const endereco = `Rua do Cleber, 420`
+// const sexo = "I"
+
+
+// let NomeCompleto = "Josias Pinto"
+// let a = 2
+// a = 3
+// a = "abc"
+
+// var c = 1+2+3
+// var d = "iubf"
+
