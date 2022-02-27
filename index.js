4a2a60a600675f64471c90d0a4b9505d34b404bc
pup0 <conrado.azzalin@uol.com.br>
2022-02-27 15:26:53 -0300

    instalado em casa

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-// let umaFuncao = function(){
-//    console.log("Fui armazenada em uma variável")
-// }
+   // let umaFuncao = function(){
+  //    console.log("Fui armazenada em uma variável")
+ // }
 // //umaFuncao(
 
 function f (funcao){
@@ -19,38 +19,38 @@ function g (){
 const gResult = g()
 console.log(gResult)
 
-// f(function (){
-//    console.log("Estou sendo passada para a f")
+  // f(function (){
+ //    console.log("Estou sendo passada para a f")
 // })
 
 // f(3)
 
-// const triplo = (n) => {
-//     return n*3
-// }
+   // const triplo = (n) => {
+  //     return n*3
+ // }
 // console.log(triplo(5))
 
-// const dobro = (n) => n*2
+ // const dobro = (n) => n*2
 // console.log(dobro (5))
 
-// const hello = () => {console.log("Hello")}
+ // const hello = () => {console.log("Hello")}
 // hello()
 
-// const triplo = function(n=10){
-//     return n*3
-// }
-// console.log(triplo(5))
-// console.log(triplo())
-// const dobro = function(n){
-//     return 2*n
-// }
-// const resultado = dobro(5)
+         // const triplo = function(n=10){
+        //     return n*3
+       // }
+      // console.log(triplo(5))
+     // console.log(triplo())
+    // const dobro = function(n){
+   //     return 2*n
+  // }
+ // const resultado = dobro(5)
 // console.log(resultado)
 
-// funtion soma(a, b){
-//     return a + b
-// }
-// const res = soma (2, 3)
+    // funtion soma(a, b){
+   //     return a + b
+  // }
+ // const res = soma (2, 3)
 // console.log(res)
 
 // //definição da função
@@ -64,151 +64,151 @@ console.log(gResult)
 //  }
 //  hello("José")
 
-// }
-// public void falar (String texto){
-//     System.out.println(texto);
-// }
-// Pessoa p = new Pessoa();
-// p.falar();
-// p.falar("texto");
+       // }
+      // public void falar (String texto){
+     //     System.out.println(texto);
+    // }
+   // Pessoa p = new Pessoa();
+  // p.falar();
+ // p.falar("texto");
 // p.falar(1);
 
-   // const valores = [1, 2, 3, 4]
-  // const soma = valores.reduce((ac, atual) => {
- //     return ac + atual
-// })
+    // const valores = [1, 2, 3, 4]
+   // const soma = valores.reduce((ac, atual) => {
+  //     return ac + atual
+ // })
 // console.log(soma)
 
 // const nome = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
 
-  // const resultado = nomes.every((n) =>{
- //      return n.startsWith("A")
-//  })
+   // const resultado = nomes.every((n) =>{
+  //      return n.startsWith("A")
+ //  })
 // console.log(resultado)
 
 //  //[A, A, R, A, C]
-  // const mapeamento = nomes.map((nome) => {
- //     return nome.charAt(0)
-// })
+   // const mapeamento = nomes.map((nome) => {
+  //     return nome.charAt(0)
+ // })
 // console.log(mapeamento)
 
-  // const apenasA = nomes.filter((n) => {
- //     return n.startsWith("A")
-// })
+   // const apenasA = nomes.filter((n) => {
+  //     return n.startsWith("A")
+ // })
 // console.log(apenasA)
 
  // arrow function \\
 // () => {}
 
- // class Pessoa{
-//     public void andar(){
+  // class Pessoa{
+ //     public void andar(){
 //     }
 
-    //     variavel = andar;
-   //     public void m(){
-  //         andar();
- //         variavel();
-//     }
+     //     variavel = andar;
+    //     public void m(){
+   //         andar();
+  //         variavel();
+ //     }
 // }
 
- // id, nome, idade
-// SELECT nome, idade, FROM pessoa
+  // id, nome, idade
+ // SELECT nome, idade, FROM pessoa
 //encontrar todos os nome que começam com 'A'
 
-//int v [] = new int [10];
+ //int v [] = new int [10];
 //List <Integer> list = new ArrayList <>();
 
-       // const v1 = []
-      // console.log(v1.length)
-     // v1[0] = 3.4
-    // v1[2] = "abc"
-   // v1[10] = 2
-  // console.log(v1.length)
- // for (let i = 0; i < v1.length; i++){
-//     console.log(v1[i])
+        // const v1 = []
+       // console.log(v1.length)
+      // v1[0] = 3.4
+     // v1[2] = "abc"
+    // v1[10] = 2
+   // console.log(v1.length)
+  // for (let i = 0; i < v1.length; i++){
+ //     console.log(v1[i])
 // }
 
 // @Deprecated 
 // Thread.stop()
 
-          // console.log(1 == 1);   → true
-         // console.log(1 == '1');   → true
-        // console.log(1 === 1);      → true
-       // console.log(1 === '1');      → false   
-      //console.log(true == 2);         → false
-     //console.log(1 == [1]);            → true
-    //console.log(1 == [1, 1]);           → false
-   //console.log(null == undefined);       → true
-  //console.log([] == false);               → true
- //console.log(typeof(Number([1,1])));       → number
-//console.log([] == []);                      → false
+           // console.log(1 == 1);   → true
+          // console.log(1 == '1');   → true
+         // console.log(1 === 1);      → true
+        // console.log(1 === '1');      → false   
+       //console.log(true == 2);         → false
+      //console.log(1 == [1]);            → true
+     //console.log(1 == [1, 1]);           → false
+    //console.log(null == undefined);       → true
+   //console.log([] == false);               → true
+  //console.log(typeof(Number([1,1])));       → number
+ //console.log([] == []);                      → false
 //console.log([] === []);                       → false
 
-  // String s1 = "abc";
- // String s2 = "abc";
-// if (s1 == s2)    //true
+   // String s1 = "abc";
+  // String s2 = "abc";
+ // if (s1 == s2)    //true
 // String s3 = JOPtionPane.showInputDialog("Digite uma string"); "abc"
 
 // if (s1 == s3) //false
 
- // = atribuição                                                    \\
-// == comparação (com coerção implícita)                             \\
+  // = atribuição                                                    \\
+ // == comparação (com coerção implícita)                             \\
 // === comparação (sem coerção implícita, compara por tipo e valor)    \\
 
 
-      // const n1 = 2;          \\
-     // const n2 = "3";          \\
-    // coerção implícita          \\
-   // const n3 = n1 + n2;          \\     
-  // console.log(n3);               \\
- // coerção explícita                \\
-// const n4 = n1 + Number(n2)         \\
+       // const n1 = 2;          \\
+      // const n2 = "3";          \\
+     // coerção implícita          \\
+    // const n3 = n1 + n2;          \\     
+   // console.log(n3);               \\
+  // coerção explícita                \\
+ // const n4 = n1 + Number(n2)         \\
 // console.log(n4);                     \\
 
-// int a = 3.5;        \\
+ // int a = 3.5;        \\
 // int a = (int) 3.5;    \\
 
-// let a = "abc";         \\
+ // let a = "abc";         \\
 // a = 2;                   \\
 
-  // let a = null;           \\
- // a = "abc";                \\  
-// console.log(a);             \\
+   // let a = null;           \\
+  // a = "abc";                \\  
+ // console.log(a);             \\
 // console.log(typeof(a))        \\
 
- // int a;                        \\
-// float b;                        \\
+  // int a;                        \\
+ // float b;                        \\
 // char c;                           \\  
 
- // String a = "abc";                 \\
-// String b = "cdf";                   \\
+  // String a = "abc";                 \\
+ // String b = "cdf";                   \\
 // int c = a-b;                          \\
 
-      // var idade = 18                   \\
-     // console.log("Oi, " + nome)         \\
-    // if (idade >= 18){                    \\
-   //      var nome = "Cleitão"              \\
-  //     console.log ("Parabéns, " + nome)    \\
- //     console.log("Você pode andar!")        \\
-// }                                            \\
+       // var idade = 18                   \\
+      // console.log("Oi, " + nome)         \\
+     // if (idade >= 18){                    \\
+    //      var nome = "Cleitão"              \\
+   //     console.log ("Parabéns, " + nome)    \\
+  //     console.log("Você pode andar!")        \\
+ // }                                            \\
 // console.log("Até logo! " + nome)               \\
 
-// var linguagem = "Javascript"                                        \\
+ // var linguagem = "Javascript"                                        \\
 // console.log ("Aprendendo" + linguagem)    - var. pode ser redeclarada \\
 
-// var linguagem = "Java"                  \\
+ // var linguagem = "Java"                  \\
 // console.log("Aprendendo" + linguagem)     \\
 
-  // const nome =  'Josias'              \\
- // const idade = 24                      \\
-// const endereco = `Rua do Cleber, 420`   \\
+   // const nome =  'Josias'              \\
+  // const idade = 24                      \\
+ // const endereco = `Rua do Cleber, 420`   \\
 // const sexo = "I"                          \\
 
-  // let NomeCompleto = "Josias Pinto"   \\
- // let a = 2                             \\
-// a = 3                                   \\
+   // let NomeCompleto = "Josias Pinto"   \\
+  // let a = 2                             \\
+ // a = 3                                   \\
 // a = "abc"                                 \\
 
-// var c = 1+2+3    \\
+ // var c = 1+2+3    \\
 // var d = "iubf"     \\
 
